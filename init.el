137e22d318acdf8ce0d10199bc0fa2075bc9eeed
techteam <techteam@dev07svr.(none)>
2014-03-20 11:01:16 -0400

    Enable ido mode

--- init.el
+++ init.el
@@ -1,9 +1,16 @@
 (require 'package)
-(add-to-list 'package-archives 
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/"))
+
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
 (package-initialize)
+
+(require 'ido)
+(ido-mode t)
 
 (load-theme 'zenburn t)
 
 (tool-bar-mode -1)
+
+(set-face-attribute 'default nil :height 120)
