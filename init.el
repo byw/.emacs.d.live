80dcdacf7e01c189930116457f83e7279ed50c41
Bobby Wang <bobby.wang@patientordersets.com>
2014-03-24 17:43:15 -0400

    Add slim and scss modes

--- init.el
+++ init.el
@@ -14,3 +14,15 @@
 (tool-bar-mode -1)
 
 (set-face-attribute 'default nil :height 120)
+
+
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq indent-line-function 'insert-tab)
