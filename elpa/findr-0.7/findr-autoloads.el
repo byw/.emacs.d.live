d92b195fc7efe10153eb7098ae43119980f84d99
Bobby Wang <wangbobby@gmail.com>
2015-02-26 12:14:51 -0500

    Update packages

--- elpa/findr-0.7/findr-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (21288 44586
-;;;;;;  975816 199000))
-
-;;;***
-
-(provide 'findr-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; findr-autoloads.el ends here
